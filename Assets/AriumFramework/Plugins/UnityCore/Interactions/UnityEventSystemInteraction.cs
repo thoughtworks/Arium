5fe41eafa911c0e479a574fb2aabedf784b901f0
lakshjain <lakshayjain93@gmail.com>
2021-10-07 19:01:26 +0530

    [Laksh] Add Generic static class to call Unity EventSystem handled interfaces/interaction

---
+++ Assets/AriumFramework/Plugins/UnityCore/Interactions/UnityEventSystemInteraction.cs
@@ -0,0 +1,30 @@
+using AriumFramework.Exceptions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace AriumFramework.Plugins.UnityCore.Interactions
+{
+    public static class UnityEventSystemInteraction<T> where T : IEventSystemHandler 
+    {
+        public static void PerformAction(GameObject gameObject, AbstractEventData eventData = null)
+        {
+            eventData ??= new PointerEventData(EventSystem.current);
+            
+            var components = gameObject.GetComponents<T>();
+            
+            if (components.Length == 0)
+            {
+                throw new ComponentNotFoundException(gameObject, typeof(T));
+            }
+            
+            var methodInfos = typeof(T).GetMethods();
+            foreach (var component in components)
+            {
+                foreach (var method in methodInfos)
+                {
+                    method.Invoke(component,new object[]{eventData});
+                }
+            }
+        }
+    }
+}
